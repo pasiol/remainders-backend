bc529faa0edac88e8a2b470f04a8f9585a433a45
Pasi Ollikainen <pasi.ollikainen@pm.me>
2021-10-20 19:42:32 +0300

    refactoring

--- models.go
+++ models.go
@@ -1,14 +1,9 @@
 package main
 
 import (
-	"context"
-	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 type remainder struct {
@@ -29,59 +24,6 @@ type User struct {
 	UpdatedAt time.Time          `bson:"updatedAt" json:"updatedAt"`
 }
 
-func searchRecipients(searchPhrase string, db *mongo.Database) ([]remainder, error) {
-	var remainders []remainder
-
-	log.Printf("Trying to find recipients filter: %s", searchPhrase)
-	options := options.Find()
-	options.SetSort(bson.D{{"updated_at", -1}})
-	options.SetLimit(200)
-
-	cursor, err := db.Collection("sended").Find(context.TODO(), bson.D{{"to", bson.D{{"$regex", searchPhrase}, {"$options", "im"}}}}, options)
-	if err != nil {
-		return []remainder{}, err
-	}
-	defer func(cursor *mongo.Cursor, ctx context.Context) {
-		err := cursor.Close(ctx)
-		if err != nil {
-			log.Printf("closing cursor failed: %s", err)
-		}
-	}(cursor, context.TODO())
-	for cursor.Next(context.TODO()) {
-		var currentRemainder remainder
-		if err = cursor.Decode(&currentRemainder); err != nil {
-			log.Printf("decoding remainder failed: err")
-		}
-
-		remainders = append(remainders, currentRemainder)
-	}
-	return remainders, nil
-}
-
-func getLatest(db *mongo.Database) ([]remainder, error) {
-	var remainders []remainder
-
-	options := options.Find()
-	options.SetSort(bson.D{{"updated_at", -1}})
-	options.SetLimit(25)
-
-	cursor, err := db.Collection("sended").Find(context.TODO(), bson.D{{}}, options)
-	if err != nil {
-		return []remainder{}, err
-	}
-	defer func(cursor *mongo.Cursor, ctx context.Context) {
-		err := cursor.Close(ctx)
-		if err != nil {
-			log.Printf("closing cursor failed: %s", err)
-		}
-	}(cursor, context.TODO())
-	for cursor.Next(context.TODO()) {
-		var currentRemainder remainder
-		if err = cursor.Decode(&currentRemainder); err != nil {
-			log.Printf("decoding remainder failed: %s", err.Error())
-		}
-		remainders = append(remainders, currentRemainder)
-	}
-	return remainders, nil
-
+type Exception struct {
+	Message string `json:"message"`
 }
