2b42c50e16e27e4ab33d4cdb04c9aeec753dadd2
Pasi Ollikainen <pasi.ollikainen@outlook.com>
2022-03-06 09:32:09 +0200

    obsolete

--- scripts/installK3d.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
--- scripts/startCluster.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-k3d cluster create --port 8082:20080@agent:0 -p 8081:80@loadbalancer --agents 2
-docker exec k3d-k3s-default-agent-0 mkdir /tmp/kube
